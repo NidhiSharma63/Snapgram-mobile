806836904d5745402b6bc06df84792d201813d00
NidhiSharma63 <nidhisharma639593@gmail.com>
2025-08-10 18:39:12 +0530

    icons added

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,51 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import colors from 'constant/color';
 import CreatePost from 'pages/CreatePostScreen';
 import Explore from 'pages/Explore';
 import HomeScreen from 'pages/HomeScreen';
 import PeopleScreen from 'pages/PeopleScreen';
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import GalleryAdd from '../src/assets/images/gallery-add.svg';
+import HomeIcon from '../src/assets/images/home.svg';
+import PeopleIcon from '../src/assets/images/people.svg';
+import Wallpaper from '../src/assets/images/wallpaper.svg';
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabs() {
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({route}) => ({
         headerShown: false,
-      }}>
+        tabBarIcon: () => {
+          if (route.name === 'Home') {
+            return <HomeIcon width={24} height={24} />;
+          }
+
+          if (route.name === 'People') {
+            return <PeopleIcon width={24} height={24} />;
+          }
+          if (route.name === 'Create Post') {
+            return <GalleryAdd width={24} height={24} />;
+          }
+          if (route.name === 'Explore') {
+            return <Wallpaper width={24} height={24} />;
+          }
+          return null;
+        },
+        tabBarActiveTintColor: colors.Primary,
+        tabBarInactiveTintColor: 'gray',
+      })}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="People" component={PeopleScreen} />
       <Tab.Screen name="Explore" component={Explore} />
       <Tab.Screen name="Create Post" component={CreatePost} />
-      {/* <Tab.Screen name="Profile" component={ProfileScreen} /> */}
     </Tab.Navigator>
   );
 }
+
 const main = () => {
   return (
     <NavigationContainer>
