4d8dd7c4cf815f4e489944d0f51d0575b1ca1411
NidhiSharma63 <nidhisharma639593@gmail.com>
2025-08-16 10:26:31 +0530

    like and save working now

--- src/components/SinglePostComponent/SinglePostComponent.tsx
+++ src/components/SinglePostComponent/SinglePostComponent.tsx
@@ -2,46 +2,28 @@ import useSinglePostComponent from 'components/SinglePostComponent/hook';
 import style from 'components/SinglePostComponent/style';
 import {formatDateTime} from 'lib/FormateDate';
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, Pressable, Text, View} from 'react-native';
+import Like from '../../assets/images/like.svg';
+import Liked from '../../assets/images/liked.svg';
 import ProfilePlaceholder from '../../assets/images/profile-placeholder.svg';
-// import Liked from '../../assets/images/Liked.svg';
-// import Saved from '../../assets/images/Saved.svg';
-// import Save from '../../assets/images/Save.svg';
-// import Like from '../../assets/images/Like.svg';
+import Save from '../../assets/images/save.svg';
+import Saved from '../../assets/images/saved.svg';
 
 const SinglePostComponent = ({id}: {id: string}) => {
-  const {data, isPostPending, usersData, findCurrentUser} =
-    useSinglePostComponent(id);
-  console.log({findCurrentUser, data});
+  const {
+    data,
+    isPostPending,
+    usersData,
+    findCurrentUser,
+    saveData,
+    likeData,
+    handleAddLike,
+    handleAddSave,
+    handleRemoveLike,
+    handleRemoveSave,
+  } = useSinglePostComponent(id);
   return (
     <View style={style.container}>
-      {/* {
-        // loading component
-        isPostPending ? (
-          <Text>Loading...</Text>
-        ) : (
-          <View style={style.mainImage}>
-            <Image source={{uri: data?.file}} style={style.image} />
-            <View>
-              <View>
-                {findCurrentUser?.avatar ? (
-                  <Image
-                    source={{uri: findCurrentUser?.avatar}}
-                    style={{width: 24, height: 24, borderRadius: 12}}
-                  />
-                ) : (
-                  <ProfilePlacholder width={24} height={24} />
-                )}
-                <View>
-                  <Text>{findCurrentUser?.username || 'Unknown'}</Text>
-                  <Text>{formatDateTime(data?.createdAt) || 'Unknown'}</Text>
-                  <Text>{data?.location?.join(', ') || 'Unknown'}</Text>
-                </View>
-              </View>
-            </View>
-          </View>
-        )
-      } */}
       {isPostPending && <Text>Loading...</Text>}
       <View style={style.postCard}>
         {/* post card header */}
@@ -84,26 +66,26 @@ const SinglePostComponent = ({id}: {id: string}) => {
           }}
         />
         {/* actions */}
-        {/* <View style={style.actions}>
-        {likeData?.[0]?.postId.includes(data._id) ? (
-          <Pressable onPress={() => handleRemoveLike(data._id)}>
-            <Liked />
-          </Pressable>
-        ) : (
-          <Pressable onPress={() => handleAddLike(data._id)}>
-            <Like />
-          </Pressable>
-        )}
-        {saveData?.[0]?.postId.includes(data._id) ? (
-          <Pressable onPress={() => handleRemoveSave(data._id)}>
-            <Saved />
-          </Pressable>
-        ) : (
-          <Pressable onPress={() => handleAddSave(data._id)}>
-            <Save />
-          </Pressable>
-        )}
-      </View> */}
+        <View style={style.actions}>
+          {likeData?.[0]?.postId.includes(data?._id) ? (
+            <Pressable onPress={() => handleRemoveLike(data?._id)}>
+              <Liked />
+            </Pressable>
+          ) : (
+            <Pressable onPress={() => handleAddLike(data?._id)}>
+              <Like />
+            </Pressable>
+          )}
+          {saveData?.[0]?.postId.includes(data?._id) ? (
+            <Pressable onPress={() => handleRemoveSave(data?._id)}>
+              <Saved />
+            </Pressable>
+          ) : (
+            <Pressable onPress={() => handleAddSave(data?._id)}>
+              <Save />
+            </Pressable>
+          )}
+        </View>
       </View>
     </View>
   );
--- src/components/SinglePostComponent/hook.ts
+++ src/components/SinglePostComponent/hook.ts
@@ -1,5 +1,8 @@
 import useAuth from 'hooks/useAuth';
+import useLikePost from 'hooks/useLikePost';
 import usePost from 'hooks/usePost';
+import useSavePost from 'hooks/useSavePost';
+import {useCallback} from 'react';
 
 const useSinglePostComponent = (id: string) => {
   const {useGetPostById} = usePost();
@@ -8,11 +11,55 @@ const useSinglePostComponent = (id: string) => {
   const {data: usersData} = useGetAllUser();
   const findCurrentUser = usersData?.find(user => user?._id === data?.userId);
 
+  const {useAddSave, useGetAllSavePost, useRemoveSave} = useSavePost();
+  const {useAddLike, useGetAllLike, useRemoveLike} = useLikePost();
+  const {data: saveData} = useGetAllSavePost();
+  const {data: likeData} = useGetAllLike();
+
+  // add like and save post
+  const {mutateAsync: addLike} = useAddLike();
+  const {mutateAsync: addSave} = useAddSave();
+  // remove like and remove save post
+  const {mutateAsync: removeLike} = useRemoveLike();
+  const {mutateAsync: removeSave} = useRemoveSave();
+
+  const handleAddLike = useCallback(
+    (postId: string) => {
+      addLike({postId: postId});
+    },
+    [addLike],
+  );
+  const handleAddSave = useCallback(
+    (postId: string) => {
+      addSave({postId: postId});
+    },
+    [addSave],
+  );
+
+  const handleRemoveLike = useCallback(
+    (postId: string) => {
+      removeLike({postId: postId});
+    },
+    [removeLike],
+  );
+
+  const handleRemoveSave = useCallback(
+    (postId: string) => {
+      removeSave({postId: postId});
+    },
+    [removeSave],
+  );
   return {
     data,
     isPostPending,
     usersData,
     findCurrentUser,
+    saveData,
+    likeData,
+    handleAddLike,
+    handleAddSave,
+    handleRemoveLike,
+    handleRemoveSave,
   };
 };
 
